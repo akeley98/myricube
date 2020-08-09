556ffd4f0443a38ade2e9dd75b83f148258034e6
akeley98 <dza724@gmail.com>
2020-08-08 23:25:22 -0700

    Add 1 random walk on startup (in random walk app).

--- apps/random-walk.cc
+++ apps/random-walk.cc
@@ -8,6 +8,27 @@ namespace myricube {
 VoxelWorld* p_world;
 std::mt19937 rng{19980724};
 
+void add_random_walk()
+{
+    uint8_t blue = rng() >> 24;
+    uint8_t red = rng() >> 24;
+    uint8_t green_base = 40 + (rng() >> 26);
+    int x = 0, y = 0, z = 0;
+    for (int i = 0; i < 200000; ++i) {
+        switch (rng() % 6) {
+            case 0: x++; break;
+            case 1: y++; break;
+            case 2: z++; break;
+            case 3: x--; break;
+            case 4: y--; break;
+            case 5: z--; break;
+        }
+        uint8_t green = uint8_t((rng() >> 25) + green_base);
+        Voxel voxel(red, green, blue);
+        p_world->set(glm::ivec3(x, y, z), voxel);
+    }
+}
+
 void app_init(VoxelWorld& world, Window& window)
 {
     p_world = &world;
@@ -15,26 +36,11 @@ void app_init(VoxelWorld& world, Window& window)
     target.down = [] (KeyArg arg) -> bool
     {
         if (arg.repeat) return false;
-        uint8_t blue = rng() >> 24;
-        uint8_t red = rng() >> 24;
-        uint8_t green_base = 40 + (rng() >> 26);
-        int x = 0, y = 0, z = 0;
-        for (int i = 0; i < 200000; ++i) {
-            switch (rng() % 6) {
-                case 0: x++; break;
-                case 1: y++; break;
-                case 2: z++; break;
-                case 3: x--; break;
-                case 4: y--; break;
-                case 5: z--; break;
-            }
-            uint8_t green = uint8_t((rng() >> 25) + green_base);
-            Voxel voxel(red, green, blue);
-            p_world->set(glm::ivec3(x, y, z), voxel);
-        }
+        add_random_walk();
         return true;
     };
     window.add_key_target("add_random_walk", target);
+    add_random_walk();
 }
 
 void app_update(VoxelWorld& world)
